88d4aaba19b89e9d6a6bdd78f1dfe0c68d5e73dc
AndersonBranco <branco.dsc@gmail.com>
2026-03-24 15:53:20 -0300

    primeiro desafio

--- exemplo_02.py
+++ exemplo_02.py
@@ -1,5 +1,6 @@
-nome = input("Digite seu nome: ")
-salario = float(input("Digite seu salário: "))
-bonus = float(input("Digite o valor do bônus: "))
-salario_final = salario + bonus
-print("O salário final de " + nome + " é: " + str(salario_final))
+BONUS_USUARIO = 1000.00
+nome_usuario = input("Digite seu nome: ")
+salario_usuario = float(input("Digite seu salário: "))
+bonus_usuario = float(input("Digite o valor do bônus: "))
+salario_usuario = BONUS_USUARIO + salario_usuario * bonus_usuario
+print(f"O salário final de {nome_usuario} é: {salario_usuario}")
